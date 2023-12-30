9efc664ba2feb8f5aa45bce56257778548ac005b
JJURIZ <jjuriz@gmail.com>
2023-12-30 13:12:41 -0500

    Remove reports module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
-import { Report } from './reports/reports.entity';
 
 @Module({
   imports: [
     UsersModule,
-    ReportsModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
